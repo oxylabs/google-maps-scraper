548d2e7b84d6ffff55caebfb6c1a6e8ee9b2e4bb
= <ignassimkunas@gmail.com>
2024-10-24 18:42:40 +0300

    Add initial scraper implementation

--- code examples/google_maps_get_results.py
+++ code examples/google_maps_get_results.py
@@ -1,10 +1,11 @@
 import requests
 
+
 # Get response.
 response = requests.request(
-    'GET',
-    'http://data.oxylabs.io/v1/queries/{job_id}/results',
-    auth=('USERNAME', 'PASSWORD')
+    "GET",
+    "http://data.oxylabs.io/v1/queries/{job_id}/results",
+    auth=("USERNAME", "PASSWORD"),
 )
 
 # This will return the JSON response with results.
